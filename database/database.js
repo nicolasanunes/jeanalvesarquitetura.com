8d56ba1337963ac06a27b308264a0c1094941bcd
Nícolas Nunes <nicolasanunes@hotmail.com>
2024-01-23 16:55:57 -0300

    validator implemented

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 const connection = new Sequelize('xuyiwh_jaarq','xuyiwh_userbd','tBz5WRkOe2', {
-    host: 'localhost',
+    host: 'mysql-ag-br1-11.conteige.cloud',
     dialect: 'mysql',
     timezone: '-03:00'
 });
